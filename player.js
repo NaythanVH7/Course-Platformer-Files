cae41c7ed8829276bddad44a327ba64234d1db2f
NaythanVH7 <n.vh7@hotmail.com>
2016-05-11 19:23:06 +1000

    changed the aim code
    updated the animation code and fixed it so the walk and idle animation
    work fine.

--- player.js
+++ player.js
@@ -107,8 +107,7 @@ Player.prototype.update = function(deltaTime)
 			this.sprite.setAnimation(ANIM_WALK_LEFT);
 		}
 	}
-	
-	if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true)
+	 else if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true)
 	{
 		right = true;
 		this.direction = RIGHT;
@@ -117,22 +116,22 @@ Player.prototype.update = function(deltaTime)
 			this.sprite.setAnimation(ANIM_WALK_RIGHT);
 		}
 	}
-	/*else 
+	else 
 	{
 		if(this.jumping == false && this.falling == false)
 		{
-			if(this.direction != LEFT)
-			{
-				if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
-					this.sprite.setAnimation(ANIM_IDLE_RIGHT);
-			}
-			else
+			if(this.direction == LEFT)
 			{
 				if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
 					this.sprite.setAnimation(ANIM_IDLE_LEFT);
 			}
+			else
+			{
+				if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+					this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+			}
 		}
-	} */
+	}
 
 	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
 	{
